1beed0628b4062cae94925e66673150c4c923502
parsa-jf <parsafares@gmail.com>
2023-06-01 22:14:43 -0700

    add new harness

---
+++ fuzz/fuzz_targets/select.rs
@@ -0,0 +1,20 @@
+#![no_main]
+
+// libfuzzer-sys crate provides the fuzzing entry point
+use libfuzzer_sys::fuzz_target;
+
+// Import the necessary types and functions from the code being fuzzed
+use kmon::kernel::log::KernelLogs;
+
+// libFuzzer entry point
+fuzz_target!(|data: &[u8]| {
+    // Create a mutable instance of KernelLogs
+    let mut kernel_logs = KernelLogs::new(); // Replace `new` with your constructor if available
+
+    // Use the input data to set the area_height and area_sub variables
+    let area_height = u16::from_le_bytes([data[0], data[1]]);
+    let area_sub = u16::from_le_bytes([data[2], data[3]]);
+
+    // Call the select function with the constructed variables
+    let _ = kernel_logs.select(area_height, area_sub);
+});
